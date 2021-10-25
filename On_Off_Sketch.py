fc5ec270843a4f2919ad015d4ae0a4576c889ded
JiamengMMa <majiamengllx@gmail.com>
2021-10-25 20:48:28 +0800

    Create On_Off_Sketch.py
    try_initial_commit

---
+++ On_Off_Sketch.py
@@ -0,0 +1,61 @@
+'''
+#CS5234-Mini-Project
+Name1: Ma Jiameng (A0198964A)
+Name2: Bennett Clement (A0200739J)
+Algorithm reference: https://github.com/Sketch-Data-Stream/On-Off-Sketch
+'''
+
+import gym
+
+
+class OO_PE(object):
+    '''
+    '''
+
+    def __init__(self, env, lanes, width, gamma):
+    
+            
+    def Abstract(self):
+        '''
+        '''
+
+    def insert(self):
+        '''
+        '''
+        
+    def Query(self):
+        '''
+        '''
+        
+    def NewWindow(self):
+        '''
+        '''
+     
+class OO_FPI(object):
+    '''
+    '''
+
+    def __init__(self, env, lanes, width, gamma):
+    
+            
+    def Abstract(self):
+        '''
+        '''
+        
+    def bucket(self):
+        '''
+        '''
+
+    def insert(self):
+        '''
+        '''
+        
+    def Query(self):
+        '''
+        '''
+        
+    def NewWindow(self):
+        '''
+        '''
+     
+
